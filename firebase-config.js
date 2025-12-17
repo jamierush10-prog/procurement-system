b7a17a7b22a4c929e588a6640ee533a105d3d6b2
jamierush10-prog <jamie.rush10@gmail.com>
2025-12-17 14:25:04 -0600

    firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// 1. IMPORT FROM WEB (CDN) LINKS
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, doc, setDoc, getDoc, getDocs, query, where, limit } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// 2. YOUR CONFIGURATION (I copied these from your screenshot)
 const firebaseConfig = {
-  apiKey: "AIzaSyCEtXeuaWJRg3PM7vXJZnRuJDfq8ldT1Tc",
+  apiKey: "AIzaSyCXetXeuaWJRg3PM7vXJZnRuJDfq8ldT1Tc",
   authDomain: "procurement-app-869b8.firebaseapp.com",
   projectId: "procurement-app-869b8",
-  storageBucket: "procurement-app-869b8.firebasestorage.app",
+  storageBucket: "procurement-app-869b8.appspot.com", 
   messagingSenderId: "546192883144",
   appId: "1:546192883144:web:2880e96220e96c3ea694fd",
   measurementId: "G-CDXJC4VQ0Q"
 };
 
-// Initialize Firebase
+// 3. INITIALIZE FIREBASE & DATABASE
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+// 4. EXPORT TOOLS (This makes them available to your other HTML pages)
+export { db, collection, doc, setDoc, getDoc, getDocs, query, where, limit };
